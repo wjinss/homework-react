7525ce66a4597fc2e74bb34f552681b6b73241a6
wjinss <aastu4343@gmail.com>
2025-08-24 20:27:32 +0900

    feat : 프래그먼트 > div로 변경

--- homework-2nd/src/components/Input.jsx
+++ homework-2nd/src/components/Input.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import Button from "./Button"
 import showPassword from '../images/showPassword.svg'
 
-export default function Input({ type, id, placeholder, label }) { 
+export default function Input({ type, id, placeholder, label, className }) { 
   const [isPasswordHide, setIsPasswordHide] = useState(true)
   const [inputType, setInputType] = useState('password')
 
@@ -12,10 +12,10 @@ export default function Input({ type, id, placeholder, label }) {
   }
 
   return (
-    <>
+    <div className={className}>
       <label htmlFor={id}>{label}</label>
       <input type={inputType} id={id} name={id} placeholder={placeholder} />
       {type === 'password' && <Button type={"button"} ariaLabel={inputType === 'password' ? "비밀번호 보기" : "비밀번호 숨기기" } onClick={handleshowPassword} ><img src={showPassword} alt="" role="none" /></ Button >}
-    </>
+    </div>
   )
 }
