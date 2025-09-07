d952effa2a6963b47929d6d57df93033be48cdea
wjinss <aastu4343@gmail.com>
2025-09-07 21:50:45 +0900

    feat : app.tsx 설정

--- homework-4th/src/app.tsx
+++ homework-4th/src/app.tsx
@@ -1,10 +1,101 @@
-import { LearnSection } from '@/components'
-import RandomCountUp from '@/demo'
+import { User } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react'
+import type { JSX } from 'react/jsx-runtime'
+import { toast } from 'sonner'
+import Navigation, { type Page } from './components/navigation'
+import usePageQuery from './hooks/use-query'
+import supabase, { type ProfilePartial } from './libs/supabase'
+import EditProfilePage from './pages/editProfile'
+import ProfilePage from './pages/profile'
+import SignInPage from './pages/sign-in'
+import SignUpPage from './pages/sign-up'
 
-export default function App() {
+const getUser = async (user: User) => {
+  return supabase
+    .from('profiles')
+    .select('username, email, bio')
+    .eq('id', user.id)
+    .single()
+}
+
+export default function AppPage() {
+  const page = usePageQuery<Page>('signup') // page값 읽어오기
+  const [user, setUser] = useState<ProfilePartial | null>(null) // 사용자 상태 : 프로필 정보 or null
+
+  useEffect(() => {
+    // 최초 마운트 시, Supabase에서 현재 사용자 정보 가져오기
+    supabase.auth.getUser().then(async ({ error, data }) => {
+      if (!error) {
+        // 인증 정보가 있으면 사용자 데이터 가져오기
+        const { error: userProfileError, data: userProfile } = await getUser(
+          data.user
+        )
+
+        if (userProfileError) {
+          // 프로필 데이터를 못가져올 경우 에러
+          toast.error(
+            `프로필 데이터 가져오기 오류 발생! ${userProfileError.message}`
+          )
+        } else {
+          // 프로필 데이터를 가져올 경우 user 상태 업데이트
+          setUser(userProfile)
+        }
+      }
+    })
+
+    // supabase 인증 상태 변경 구독
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange(async (event, session) => {
+      switch (event) {
+        case 'SIGNED_IN': {
+          // 로그인 성공하면 세션에서 user 가져오기
+          const user = session?.user
+          if (user) {
+            // 해당 user의 profiles 정보 가져오기
+            const { data } = await getUser(user)
+            setUser(data)
+          }
+          break
+        }
+        case 'SIGNED_OUT':
+          // 로그아웃하면 user의 상태를 null로 변경
+          setUser(null)
+          break
+      }
+    })
+
+    return () => {
+      // [실습] Supabase 인증 구독 해제
+      subscription.unsubscribe()
+    }
+  }, [])
+
+  let renderPage: JSX.Element | null = null
+
+  // 현재 페이지 값에 따라 동적 렌더링
+  switch (page.data) {
+    case 'signin':
+      renderPage = <SignInPage />
+      break
+    case 'signup':
+      renderPage = <SignUpPage />
+      break
+    case 'profile':
+      renderPage = <ProfilePage user={user} />
+      break
+    case 'editProfile':
+      renderPage = <EditProfilePage user={user} />
+      break
+  }
+
   return (
-    <LearnSection title="랜덤 카운트 업">
-      <RandomCountUp />
-    </LearnSection>
+    <div>
+      <header>
+        <h1>4주차 과제</h1>
+      </header>
+      <Navigation user={user} />
+      <main>{renderPage}</main>
+    </div>
   )
 }
