1e9a3b9f83bbd5fe8469d46a7eabb1a7777bcd31
wjinss <aastu4343@gmail.com>
2025-09-07 21:51:33 +0900

    feat : 각 페이지 컴포넌트 생성

---
+++ homework-4th/src/pages/editProfile.tsx
@@ -0,0 +1,3 @@
+export default function EditProfilePage({ user }: Props) {
+  return <div>프로필 변경</div>
+}
---
+++ homework-4th/src/pages/profile.tsx
@@ -0,0 +1,3 @@
+export default function ProfilePage({ user }: Props) {
+  return <div>프로필</div>
+}
